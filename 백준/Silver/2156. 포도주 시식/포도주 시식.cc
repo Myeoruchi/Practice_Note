97211bbcb5766911a2f28cd0fa9ef285271a5940
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2024-11-16 20:31:54 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    int arr[n], sum[n];
+    
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    sum[0] = arr[0];
+    sum[1] = arr[1] + arr[0];
+    sum[2] = arr[2] + max(arr[0], arr[1]);
+    sum[3] = arr[3] + max(arr[2] + arr[0], sum[1]);
+    
+    for (int i = 4; i < n; i++) {
+        sum[i] = arr[i] + max(arr[i-1] + max(sum[i-3], sum[i-4]), max(sum[i-2], sum[i-3]));
+    }
+    
+    cout << max(sum[n-1], sum[n-2]);
+    return 0;
+}
