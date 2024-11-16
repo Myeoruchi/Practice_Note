d675dd7e08528183f64a52ec22b1ae3c0378e9bb
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2024-11-16 18:13:40 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool used[10];
+
+void back(int cnt) {
+    if (cnt == m) {
+        for (int i = 1; i <= m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        if (!used[i] && arr[cnt] < i) {
+            arr[cnt+1] = i;
+            used[i] = true;
+            back(cnt+1);
+            used[i] = false;
+        }
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n >> m;
+    back(0);
+}
