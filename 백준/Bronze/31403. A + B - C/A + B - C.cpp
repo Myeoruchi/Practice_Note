c519b0be68465080c7f5e3a91fcef671017c40b5
Jo Jueun <yyi07015@naver.com>
2024-11-16 18:17:51 +0900

    trash file delete

--- 백준/Bronze/31403. A + B - C/A + B - C.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cin.sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n1, n2, n3;
-    cin >> n1 >> n2 >> n3;
-    cout << n1+n2-n3 << '\n' << stoi(to_string(n1) + to_string(n2))-n3;
-}
