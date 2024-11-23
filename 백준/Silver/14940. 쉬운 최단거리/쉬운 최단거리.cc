ff0903def149a53f1667beab29e4a3f972a8811b
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2024-11-23 22:37:00 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 36 ms, Memory: 6924 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+int arr[1002][1002];
+bool visit[1002][1002];
+int n, m;
+
+bool inRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < m;
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    
+    queue<pair<int, int>> q;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 2) {
+                q.push({i, j});
+                visit[i][j] = true;
+                arr[i][j] = 0;
+            }
+        }
+    }
+    
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (inRange(nx, ny) && visit[nx][ny] == false && arr[nx][ny] == 1) {
+                q.push({nx, ny});
+                visit[nx][ny] = true;
+                arr[nx][ny] = arr[x][y] + 1;
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (visit[i][j] == false && arr[i][j] != 0) { cout << "-1 "; }
+            else { cout << arr[i][j] << ' '; }
+        }
+        cout << '\n';
+    }
+}
