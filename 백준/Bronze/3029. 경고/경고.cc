dbf6fa50095b4e9ecd27bab154519022d17e0ec0
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2024-12-13 03:28:57 +0900

    [Bronze III] Title: 경고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3029. 경고/경고.cc
+++ 백준/Bronze/3029. 경고/경고.cc
@@ -2,34 +2,14 @@
 using namespace std;
 
 int main() {
-    int temp, sec1 = 0, sec2 = 0;
+    int h1, h2, m1, m2, s1, s2;
+    scanf("%d:%d:%d", &h1, &m1, &s1);
+    scanf("%d:%d:%d", &h2, &m2, &s2);
     
-    cin >> temp;
-    for (int i = 0; i < 2; i++) {
-        sec1 += temp;
-        sec1 *= 60;
-        cin.get();
-        cin >> temp;
-    }
-    sec1 += temp;
-    cin >> temp;
-    for (int i = 0; i < 2; i++) {
-        sec2 += temp;
-        sec2 *= 60;
-        cin.get();
-        cin >> temp;
-    }
-    sec2 += temp;
+    s1 += h1 * 3600 + m1 * 60;
+    s2 += h2 * 3600 + m2 * 60;
+    int res = s1 >= s2 ? 3600 * 24 + s2 - s1 : s2 - s1;
     
-    int res;
-    if (sec1 >= sec2) { res = 60*60*24 - sec1 + sec2; }
-    else { res = sec2 - sec1; }
-    
-    int h = res / (60 * 60);
-    res %= 60 * 60;
-    int m = res / 60;
-    res %= 60;
-    
-    printf("%02d:%02d:%02d", h, m, res);
+    printf("%02d:%02d:%02d", res / 3600, (res / 60) % 60, res % 60);
     return 0;
 }
