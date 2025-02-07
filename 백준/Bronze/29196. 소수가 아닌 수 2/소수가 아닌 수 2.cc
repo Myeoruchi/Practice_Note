195804847653e81db7797c1f3fed2143a750232d
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-02-07 14:37:46 +0900

    [Bronze I] Title: 소수가 아닌 수 2, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cc
+++ 백준/Bronze/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cc
@@ -4,9 +4,12 @@ using namespace std;
 int main() {
     string p, q = "1";
     cin >> p;
-    p.erase(0, 2);
-    for (int i = 0; i < p.length(); i++) {
+    
+    int pos = 2;
+    for (int i = 0; i < p.length() - 2; i++) {
+        if (pos == i+2 && p[i+2] == '0') { pos++; }
         q += "0";
     }
+    p.erase(0, pos);
     cout << "YES\n" << p << ' ' << q;
 }
