d11a582be782375216d9be317dc18ca78f6f6146
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-02-17 03:34:44 +0900

    [Silver IV] Title: 수들의 합 2, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += arr[j];
+            if (sum == m) { cnt++; }
+            else if (sum > m) { break; }
+        }
+    }
+    
+    cout << cnt;
+}
