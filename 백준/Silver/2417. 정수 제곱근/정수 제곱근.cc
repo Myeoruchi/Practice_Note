76647986bfcc2ced3a81ec851ebb8dc70dfb51ff
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-02 02:45:18 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -1,14 +1,19 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 #define FAST_IO ios::sync_with_stdio(0), cin.tie(0)
 
 int main() {
     FAST_IO;
-    long long n, q;
+    unsigned long long n, left = 0, right = 1LL << 32 , mid;
     cin >> n;
-    q = sqrt(n);
-    if (q * q < n) { q++; }
-    cout << q;
+    while (left < right) {
+        mid = (left + right) >> 1;
+        if (mid * mid >= n) { right = mid; }
+        else { left = mid + 1; }
+    }
+    cout << left;
 }
+
+/* unsigned이기 때문에 left <= right, right = mid - 1로 했을 때
+0일 때 무한루프에 빠짐 */
