31f7e470a5cea259d6ef1d02d34ef0a09021e2a7
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-01 22:14:58 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.cc
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.cc
@@ -7,13 +7,9 @@ int main() {
     FAST_IO;
     string a, b;
     cin >> a >> b;
-    if (a == "0" || b == "0") { cout << 0; return 0; }
     
-    long long ans = 0;
-    for (const auto& n1: a) {
-        for (const auto& n2: b) {
-            ans += (n1 - '0') * (n2 - '0');
-        }
-    }
-    cout << ans;
+    long long n1 = 0, n2 = 0;
+    for (const auto& n: a) { n1 += n - '0'; }
+    for (const auto& n: b) { n2 += n - '0'; }
+    cout << n1 * n2;
 }
