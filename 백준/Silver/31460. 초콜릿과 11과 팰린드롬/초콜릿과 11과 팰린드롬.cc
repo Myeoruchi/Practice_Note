6fb2bf3c11db09d307fca5f8fb7d84dfb26b1ca2
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-08 11:48:10 +0900

    [Silver IV] Title: 초콜릿과 11과 팰린드롬, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/31460. 초콜릿과 11과 팰린드롬/초콜릿과 11과 팰린드롬.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(0), cin.tie(0)
+
+int main() {
+    FAST_IO;
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        string ans = "0";
+        while (--n) {
+            ans[ans.length()-1] = ans[ans.length()-1] + 1;
+            ans += '1';
+        }
+        cout << ans << '\n';
+    }
+}
