5a94bf29a8e08ef8ce156971fada779bc0e3aa78
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-11 16:07:27 +0900

    [Silver IV] Title: 마지막 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/28110. 마지막 문제/마지막 문제.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(0), cin.tie(0)
+
+int main() {
+    FAST_IO;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr+n);
+    
+    int size = 1, ans = -1;
+    for (int i = 0; i < n-1; i++) {
+        if ((size >> 1) < (arr[i+1] - arr[i]) >> 1) {
+            size = arr[i+1] - arr[i];
+            ans = arr[i] + (size >> 1);
+        }
+    }
+    cout << ans;
+}
