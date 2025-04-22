34dfe58d951e36d181200a4b9182215ebdc1f174
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-22 20:20:06 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -3,17 +3,17 @@ using namespace std;
 
 #define FAST_IO ios::sync_with_stdio(0), cin.tie(0)
 
-int arr[12];
+int arr[11];
 
 int dp(int n) {
     if (arr[n]) { return arr[n]; }
-    return arr[n] = dp(n-1) * 2 - dp(n-4);
+    return arr[n] = dp(n-1) + dp(n-2) + dp(n-3);
 }
 
 int main() {
     int t, n;
     cin >> t;
-    arr[1] = 1, arr[2] = 2, arr[3] = 4, arr[4] = 7;
+    arr[1] = 1, arr[2] = 2, arr[3] = 4;
     while (t--) {
         cin >> n;
         cout << dp(n) << '\n';
