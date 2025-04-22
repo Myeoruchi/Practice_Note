5b70ee826b95b0237e10fe416235af966b33e3a6
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-22 19:22:07 +0900

    [Silver II] Title: 더 흔한 타일 색칠 문제, Time: 16 ms, Memory: 3148 KB -BaekjoonHub

--- 백준/Silver/28298. 더 흔한 타일 색칠 문제/더 흔한 타일 색칠 문제.cc
+++ 백준/Silver/28298. 더 흔한 타일 색칠 문제/더 흔한 타일 색칠 문제.cc
@@ -10,20 +10,21 @@ int main() {
     int n, m, k;
     cin >> n >> m >> k;
     vector<string> v(n);
-    vector<vector<vector<int>>> cnt(k, vector<vector<int>> (k, vector<int> (26, 0)));
     for (auto &s: v) { cin >> s; }
     
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cnt[i % k][j % k][v[i][j] - 'A']++;
-        }
-    }
-    int ans = 0, max = (n / k) * (m / k), idx[k][k];
+    int ans = 0, max = (n / k) * (m / k);
+    int cnt[26], idx[k][k];
     for (int i = 0; i < k; i++) {
         for (int j = 0; j < k; j++) {
-            auto tmp = max_element(cnt[i][j].begin(), cnt[i][j].end());
+            for (auto &e: cnt) { e = 0; }
+            for (int p = i; p < n; p += k) {
+                for (int q = j; q < m; q += k) {
+                    cnt[v[p][q] - 'A']++;
+                }
+            }
+            auto tmp = max_element(cnt, cnt + 26);
             ans += max - *tmp;
-            idx[i][j] = tmp - cnt[i][j].begin();
+            idx[i][j] = tmp - cnt;
         }
     }
     cout << ans << '\n';
