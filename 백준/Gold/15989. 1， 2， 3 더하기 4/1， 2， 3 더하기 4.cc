cdfa597bdee926c3c289727866ac3b0e9267888c
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-22 21:20:56 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -1,24 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 #define FAST_IO ios::sync_with_stdio(0), cin.tie(0)
-int arr[1000001];
+int arr[10001];
 
 int main() {
-    int t, n;
-    cin >> t;
-    vector<int> v(t);
-    for (int &e: v) { cin >> e; }
-    int max = *max_element(v.begin(), v.end());
-    
+    FAST_IO;
     arr[1] = 1, arr[2] = 2, arr[3] = 3;
-    for (int i = 4; i <= max; i++) {
+    for (int i = 4; i <= 10000; i++) {
         arr[i] = arr[i-2] + arr[i-1] - arr[i-3] + !(i % 3);
     }
     
-    for (const int &e: v) {
-        cout << arr[e] << '\n';
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cout << arr[n] << '\n';
     }
 }
