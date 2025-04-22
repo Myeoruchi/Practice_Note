4df5772079fdaa92af93952f639dccda0eb60c36
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-22 20:39:41 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 using ll = long long;
 
@@ -6,17 +8,19 @@ using ll = long long;
 #define MOD 1000000009
 ll arr[1000001];
 
-ll dp(int n) {
-    if (arr[n]) { return arr[n]; }
-    return arr[n] = (dp(n-1) + dp(n-2) + dp(n-3)) % MOD;
-}
-
 int main() {
     int t, n;
     cin >> t;
+    vector<int> v(t);
+    for (int &e: v) { cin >> e; }
+    int max = *max_element(v.begin(), v.end());
+    
     arr[1] = 1, arr[2] = 2, arr[3] = 4;
-    while (t--) {
-        cin >> n;
-        cout << dp(n) << '\n';
+    for (int i = 4; i <= max; i++) {
+        arr[i] = (arr[i-1] + arr[i-2] + arr[i-3]) % MOD;
+    }
+    
+    for (const int &e: v) {
+        cout << arr[e] << '\n';
     }
 }
