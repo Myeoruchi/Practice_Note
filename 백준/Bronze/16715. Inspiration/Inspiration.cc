654a9e6290cfc183868f9c75444ddaaf140ebc5f
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-23 13:09:45 +0900

    [Bronze I] Title: Inspiration, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/16715. Inspiration/Inspiration.cc
+++ 백준/Bronze/16715. Inspiration/Inspiration.cc
@@ -1,12 +1,7 @@
-#include <iostream>
-using namespace std;
-
-#define FAST_IO ios::sync_with_stdio(0), cin.tie(0)
-
+#include <stdio.h>
 int main() {
-    FAST_IO;
     int n;
-    cin >> n;
-    if (n == 8) { cout << "4 3"; }
-    else { cout << n - n / 2 << ' ' << n / 2 + 1; }
+    scanf("%d", &n);
+    if (n == 8) { printf("4 3"); }
+    else { printf("%d %d", n-n/2, n/2+1); }
 }
