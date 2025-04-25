ef96c7d003654ae7cf42aa69916bbd80eb9258a4
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-25 23:00:07 +0900

    [Silver I] Title: 싫은데요, Time: 60 ms, Memory: 3852 KB -BaekjoonHub

--- 백준/Silver/25916. 싫은데요/싫은데요.cc
+++ 백준/Silver/25916. 싫은데요/싫은데요.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 #define IO ios::sync_with_stdio(0), cin.tie(0)
@@ -8,7 +7,7 @@ int main() {
     IO;
     int n, m, arr[500000];
     cin >> n >> m;
-    for (int &e: arr) { cin >> e; }
+    for (int i = 0; i < n; i++) { cin >> arr[i]; }
     
     int ans = 0, sum = 0;
     for (int i = 0, j = 0; i < n; i++) {
