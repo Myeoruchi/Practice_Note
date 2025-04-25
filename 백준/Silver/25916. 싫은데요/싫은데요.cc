75a80d05a15fa3b2a33fc5c37a423982353a94d1
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-25 22:57:58 +0900

    [Silver I] Title: 싫은데요, Time: 60 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/25916. 싫은데요/싫은데요.cc
+++ 백준/Silver/25916. 싫은데요/싫은데요.cc
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
-using ll = long long;
+
 #define IO ios::sync_with_stdio(0), cin.tie(0)
 
 int main() {
     IO;
-    int n, m, idx = 0;
+    int n, m;
     cin >> n >> m;
-    vector<ll> v(n), ans(n);
-    for (ll &e: v) { cin >> e; }
-    for (int i = 0; i < n; i++) {
-        if (i && ans[i-1]) { ans[i] = ans[i-1] - v[i-1]; }
-        if (i > idx) { idx = i; }
-        for (idx; idx < n; idx++) {
-            if (ans[i] + v[idx] > m) { break; }
-            ans[i] += v[idx];
-        }
+    vector<int> v(n);
+    for (int &e: v) { cin >> e; }
+    
+    int ans = 0, sum = 0;
+    for (int i = 0, j = 0; i < n; i++) {
+        sum += v[i];
+        while (sum > m) { sum -= v[j++]; }
+        ans = max(ans, sum);
     }
-    cout << *max_element(ans.begin(), ans.end());
+    cout << ans;
 }
