4628e2fdabc48c63d627e94c0d2f8a53bb01fdd4
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-25 23:01:56 +0900

    [Silver I] Title: 싫은데요, Time: 60 ms, Memory: 3852 KB -BaekjoonHub

--- 백준/Silver/25916. 싫은데요/싫은데요.cc
+++ 백준/Silver/25916. 싫은데요/싫은데요.cc
@@ -7,10 +7,10 @@ int main() {
     IO;
     int n, m, arr[500000];
     cin >> n >> m;
-    for (int i = 0; i < n; i++) { cin >> arr[i]; }
+    for (int i = 0; i < n; ++i) { cin >> arr[i]; }
     
     int ans = 0, sum = 0;
-    for (int i = 0, j = 0; i < n; i++) {
+    for (int i = 0, j = 0; i < n; ++i) {
         sum += arr[i];
         while (sum > m) { sum -= arr[j++]; }
         ans = max(ans, sum);
