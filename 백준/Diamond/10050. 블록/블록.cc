05ec035b379452f26c04d4561b705e07768babd3
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-04-29 01:27:21 +0900

    [Diamond I] Title: 블록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Diamond/10050. 블록/블록.cc
+++ 백준/Diamond/10050. 블록/블록.cc
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #define IO ios::sync_with_stdio(0), cin.tie(0)
+
+vector<vector<pair<int, int>>> v(8);
 
 void solve(int n, int depth) {
     int diff = depth * 4;
@@ -10,60 +13,24 @@ void solve(int n, int depth) {
         cout << 3 + diff << " to " << 2 * (n - 1) + diff << '\n';
         solve(n - 4, depth + 1);
     }
-    else if (n == 7) {
-        cout << 8 + diff << " to " << -1 + diff << '\n';
-        cout << 5 + diff << " to " << 8 + diff << '\n';
-        cout << 12 + diff << " to " << 5 + diff << '\n';
-        cout << 3 + diff << " to " << 12 + diff << '\n';
-        cout << 9 + diff << " to " << 3 + diff << '\n';
-        cout << 0 + diff << " to " << 9 + diff << '\n';
-        cout << 13 + diff << " to " << 0 + diff << '\n';
-        for (int i = 0; i < depth; i++) {
-            cout << 0 + diff - 4 * (i + 1) << " to " << 13 + diff + 4 * i << '\n';
-            cout << 13 + diff + 4 * (i + 1) << " to " << 0 + diff - 4 * (i + 1) << '\n';
-        }
-    }
-    else if (n == 6) {
-        cout << 10 + diff << " to " << -1 + diff << '\n';
-        cout << 7 + diff << " to " << 10 + diff << '\n';
-        cout << 2 + diff << " to " << 7 + diff << '\n';
-        cout << 6 + diff << " to " << 2 + diff << '\n';
-        cout << 0 + diff << " to " << 6 + diff << '\n';
-        cout << 11 + diff << " to " << 0 + diff << '\n';
-        for (int i = 0; i < depth; i++) {
-            cout << 0 + diff - 4 * (i + 1) << " to " << 11 + diff + 4 * i << '\n';
-            cout << 11 + diff + 4 * (i + 1) << " to " << 0 + diff - 4 * (i + 1) << '\n';
-        }
-    }
-    else if (n == 5) {
-        cout << 8 + diff << " to " << -1 + diff << '\n';
-        cout << 3 + diff << " to " << 8 + diff << '\n';
-        cout << 6 + diff << " to " << 3 + diff << '\n';
-        cout << 0 + diff << " to " << 6 + diff << '\n';
-        cout << 9 + diff << " to " << 0 + diff << '\n';
-        for (int i = 0; i < depth; i++) {
-            cout << 0 + diff - 4 * (i + 1) << " to " << 9 + diff + 4 * i << '\n';
-            cout << 9 + diff + 4 * (i + 1) << " to " << 0 + diff - 4 * (i + 1) << '\n';
-        }
-    }
-    else if (n == 4) {
-        cout << 6 + diff << " to " << -1 + diff << '\n';
-        cout << 3 + diff << " to " << 6 + diff << '\n';
-        cout << 0 + diff << " to " << 3 + diff << '\n';
-        cout << 7 + diff << " to " << 0 + diff << '\n';
-        for (int i = 0; i < depth; i++) {
-            cout << 0 + diff - 4 * (i + 1) << " to " << 7 + diff + 4 * i << '\n';
-            cout << 7 + diff + 4 * (i + 1) << " to " << 0 + diff - 4 * (i + 1) << '\n';
-        }
-    }
     else {
-        cout << 2 + diff << " to " << -1 + diff << '\n';
-        cout << 5 + diff << " to " << 2 + diff << '\n';
-        cout << 3 + diff << " to " << -3 + diff << '\n';
+        for (auto &e: v[n]) {
+            cout << e.first + diff << " to " << e.second + diff << '\n';
+        }
+        for (int i = 0; i < depth; i++) {
+            cout << diff - 4 * (i + 1) << " to " << v[n].back().first + diff + 4 * i << '\n';
+            cout << v[n].back().first + diff + 4 * (i + 1) << " to " << diff - 4 * (i + 1) << '\n';
+        }
     }
 }
 
 int main() {
+    v[3] = {{2, -1}, {5, 2}, {3, -3}};
+    v[4] = {{6, -1}, {3, 6}, {0, 3}, {7, 0}};
+    v[5] = {{8, -1}, {3, 8}, {6, 3}, {0, 6}, {9, 0}};
+    v[6] = {{10, -1}, {7, 10}, {2, 7}, {6, 2}, {0, 6}, {11, 0}};
+    v[7] = {{8, -1}, {5, 8}, {12, 5}, {3, 12}, {9, 3}, {0, 9}, {13, 0}};
+    
     int n;
     cin >> n;
     solve(n, 0);
