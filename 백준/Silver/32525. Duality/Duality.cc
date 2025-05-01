7abbde1e423e624247e6f3d0cf68d8530b0b589a
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-05-01 22:28:20 +0900

    [Silver I] Title: Duality, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/32525. Duality/Duality.cc
+++ 백준/Silver/32525. Duality/Duality.cc
@@ -1,31 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 #define IO ios::sync_with_stdio(0), cin.tie(0)
 
-struct point { int x, y, idx; };
-
-bool comp(point &a, point &b) {
-    if (a.x != b.x) { return a.x < b.x; }
-    return a.y < b.y;
-}
-
 int main() {
     IO;
-    int t, n, diff = -1e9;
+    int t, n, diff = 2e8 + 1;
     cin >> t;
     while (t--) {
         cin >> n;
-        vector<point> v(n);
+        vector<pair<int, int>> v(n);
+        for (auto &e: v) { cin >> e.first >> e.second; }
         for (int i = 0; i < n; i++) {
-            cin >> v[i].x >> v[i].y;
-            v[i].idx = i + 1;
-        }
-        sort(v.begin(), v.end(), comp);
-        for (int i = 0; i < n; i++) {
-            cout << v[i].idx << ' ' << v[i].x + 1 << ' ' << diff++ << '\n';
+            cout << i + 1 << ' ' << v[i].first + 1 << ' ' << v[i].second + diff << '\n';
         }
     }
 }
