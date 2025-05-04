a2eaa75e4615fd2452a048a38af0a32cd6c57ffe
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-05-04 20:26:15 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -3,20 +3,16 @@ using namespace std;
 using ll = long long;
 
 #define IO ios::sync_with_stdio(0), cin.tie(0)
-ll arr[31];
 
 int main() {
     IO;
-    int a, b, c;
+    ll a, b, c;
     ll ans = 1;
     cin >> a >> b >> c;
-    arr[0] = a % c;
-    for (int i = 1; i < 31; i++) {
-        arr[i] = ((arr[i-1] % c) * (arr[i-1] % c)) % c;
-    }
-    for (int i = 30; i >= 0; i--) {
-        ans *= (b >> i) & 1 ? arr[i] : 1;
-        ans %= c;
+    while (b) {
+        if (b & 1) { ans = (ans * a) % c; }
+        a = (a * a) % c;
+        b >>= 1;
     }
     cout << ans;
 }
