6f8b938e33ffc3c8b98496c6511a5274c3a88a66
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-05-13 18:52:05 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 80 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define IO ios::sync_with_stdio(0), cin.tie(0)
+
+int main() {
+    IO;
+    int n, p;
+    cin >> n >> p;
+    vector<vector<int>> v(n+1);
+    
+    int cnt = 0, line, fret;
+    while (n--) {
+        cin >> line >> fret;
+        if (v[line].empty()) { v[line].emplace_back(fret); }
+        else {
+            while (!v[line].empty() && v[line].back() > fret) {
+                v[line].pop_back();
+                cnt++;
+            }
+            if (v[line].empty() || v[line].back() != fret) {
+                v[line].emplace_back(fret);
+            }
+            else { cnt--; }
+        }
+        cnt++;
+    }
+    cout << cnt;
+}
