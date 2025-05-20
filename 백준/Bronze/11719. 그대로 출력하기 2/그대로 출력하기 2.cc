0c265a45864b5957ecb7300abc3b888536667723
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-05-20 22:57:10 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+#define IO cin.tie(0)->sync_with_stdio(0)
+
+int main() {
+    IO;
+    string s;
+    while (getline(cin, s)) { cout << s << '\n'; }
+}
