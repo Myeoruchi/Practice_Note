bcf945a1ad864f0acdf97fa91738d2be226e3543
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-05-21 00:29:47 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.cc
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -3,23 +3,17 @@ using namespace std;
 
 #define IO cin.tie(0)->sync_with_stdio(0)
 
-int dp[1001][10];
+int dp[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 int main() {
     IO;
     int n; cin >> n;
-    for (int i = 0; i <= 9; i++) { dp[1][i] = 1; }
     for (int i = 2; i <= n; i++) {
+        int tmp = 0;
         for (int j = 0; j <= 9; j++) {
-            for (int k = 0; k <= j; k++) {
-                dp[i][j] += (dp[i-1][k]) % 10007;
-            }
+            tmp = (tmp + dp[j]) % 10007;
+            dp[j] = tmp;
         }
     }
-    
-    int sum = 0;
-    for (int &e: dp[n]) {
-        sum = (sum + e) % 10007;
-    }
-    cout << sum;
+    cout << dp[9];
 }
