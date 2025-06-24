93eee78b9a13cf54f72999efbc91c9ffcf414b68
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-06-25 04:56:08 +0900

    [Bronze V] Title: Serca, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+#define IO cin.tie(0)->sync_with_stdio(0)
+
+int main() {
+    IO;
+    int n; cin >> n;
+    while (n--) {
+        cout << " @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n";
+    }
+}
