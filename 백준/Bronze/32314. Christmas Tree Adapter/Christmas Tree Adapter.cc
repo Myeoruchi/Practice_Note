427de3b837894ebcfd2293c55f056f2d6c992573
Jueun Jo <50043813+Myeoruchi@users.noreply.github.com>
2025-07-21 02:39:11 +0900

    [Bronze V] Title: Christmas Tree Adapter, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32314. Christmas Tree Adapter/Christmas Tree Adapter.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+#define IO cin.tie(0)->sync_with_stdio(0)
+
+int main() {
+    IO;
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << (a <= b / c);
+}
